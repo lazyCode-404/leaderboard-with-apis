acc31de6ca0506acfffaff9752e3d968ae4569d6
siansandatamara <tamarasiansanda18@gmail.com>
2022-05-18 22:43:38 +0200

    run linters

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import './index.css';
 
 const URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1N0LPVIoQdgSBrgOYWr4/scores/';
@@ -9,47 +8,44 @@ const player = document.querySelector('.name');
 const scores = document.querySelector('.score');
 const box = document.querySelector('.results');
 
-
 const postData = () => {
-    submitBtn.addEventListener('click', (e) => {
-        e.preventDefault();
+  submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
 
-        fetch(URL, {
-            method: 'POST',
-            body: JSON.stringify({user: player.value, score: scores.value }),
-            headers: {
-            'Content-type': 'application/json',
-            }
-        })
-        player.value = ''
-        scores.value = ''
-    })
-    // submitBtn.reset;
- 
-}
+    fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify({ user: player.value, score: scores.value }),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+    player.value = '';
+    scores.value = '';
+  });
+  // submitBtn.reset;
+};
 
 const getData = async () => {
-    const response = await fetch(URL);
-    const result = response.json();
-    // location.reload()
-    return result;
+  const response = await fetch(URL);
+  const result = response.json();
+  // location.reload()
+  return result;
+};
 
-}
-
-refreshBtn.addEventListener('click', getData)
-refreshBtn.addEventListener('click',()=> {
-    window.location.reload()
-})
+refreshBtn.addEventListener('click', getData);
+refreshBtn.addEventListener('click', () => {
+  window.location.reload();
+});
 postData();
 
-const data = async() => {
-    let data = await getData()
-    const results = data.result
+const data = async () => {
+  const data = await getData();
+  const results = data.result;
 
-    results.map((result) => {
-    box.innerHTML +=
-        `<li>${result.user}:${result.score}</li>`
-    }).join('')
-}
+  results.map.await((result) => {
+    box.innerHTML
+        += `<li>${result.user}:${result.score}</li>`;
+  }).join('');
+};
 
-data()
+data();
