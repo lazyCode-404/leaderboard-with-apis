e034b55166216b8095973f92f6e52bab34affe7e
siansandatamara <tamarasiansanda18@gmail.com>
2022-05-19 00:26:50 +0200

    fixed linters

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ const data = async () => {
   const data = await getData();
   const results = data.result;
 
-  results.map.await((result) => {
+  results.map((result) => {
     box.innerHTML
         += `<li>${result.user}:${result.score}</li>`;
   }).join('');
